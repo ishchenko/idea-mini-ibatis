e2212d2650028576f9f7ea5630d9ced50fb43b1c
Max Ishchenko <max.ishchenko@gmail.com>
2011-12-28 00:36:12 +0400

    Way faster, using DomService.getFileElements to locate sqlMap files. DomModel-related infrastructure removed.

--- src/net/ishchenko/idea/minibatis/IdentifiableStatementReference.java
+++ src/net/ishchenko/idea/minibatis/IdentifiableStatementReference.java
@@ -1,16 +1,14 @@
 package net.ishchenko.idea.minibatis;
 
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
-import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiLiteralExpression;
 import com.intellij.psi.PsiReferenceBase;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.xml.DomFileElement;
+import com.intellij.util.xml.DomService;
 import net.ishchenko.idea.minibatis.model.IdentifiableStatement;
 import net.ishchenko.idea.minibatis.model.SqlMap;
-import net.ishchenko.idea.minibatis.model.SqlMapModel;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -36,24 +34,16 @@ public class IdentifiableStatementReference extends PsiReferenceBase<PsiLiteralE
         if (parts.length != 2) {
             return null;
         }
-        
+
         String namespace = parts[0];
         String id = parts[1];
 
-        Module module = ModuleUtil.findModuleForPsiElement(getElement());
-        if (module == null) {
-            return null;
-        }
-
-        SqlMapModelFactory sqlMapModelFactory = ServiceManager.getService(getElement().getProject(), SqlMapModelFactory.class);
-        for (SqlMapModel sqlMapModel : sqlMapModelFactory.getAllModels(module)) {
-            for (DomFileElement<SqlMap> root : sqlMapModel.getRoots()) {
-                SqlMap sqlMap = root.getRootElement();
-                if (namespace.equals(sqlMap.getNamespace().getStringValue())) {
-                    for (IdentifiableStatement statement : sqlMap.getIdentifiableStatements()) {
-                        if (id.equals(statement.getId().getStringValue())) {
-                            return statement.getXmlElement();
-                        }
+        for (DomFileElement<SqlMap> fileElement : findSqlMapFileElements()) {
+            SqlMap sqlMap = fileElement.getRootElement();
+            if (namespace.equals(sqlMap.getNamespace().getRawText())) {
+                for (IdentifiableStatement statement : sqlMap.getIdentifiableStatements()) {
+                    if (id.equals(statement.getId().getRawText())) {
+                        return statement.getXmlElement();
                     }
                 }
             }
@@ -65,29 +55,28 @@ public class IdentifiableStatementReference extends PsiReferenceBase<PsiLiteralE
     @NotNull
     public Object[] getVariants() {
 
-        Module module = ModuleUtil.findModuleForPsiElement(getElement());
-        if (module == null) {
-            return EMPTY_ARRAY;
-        }
-
         List<Object> result = new ArrayList<Object>();
-        SqlMapModelFactory sqlMapModelFactory = ServiceManager.getService(getElement().getProject(), SqlMapModelFactory.class);
-        for (SqlMapModel sqlMapModel : sqlMapModelFactory.getAllModels(module)) {
-            for (DomFileElement<SqlMap> root : sqlMapModel.getRoots()) {
-                SqlMap sqlMap = root.getRootElement();
-                String namespace = sqlMap.getNamespace().getStringValue();
-                if (namespace != null) {
-                    for (IdentifiableStatement statement : sqlMap.getIdentifiableStatements()) {
-                        String id = statement.getId().getStringValue();
-                        if (id != null) {
-                            result.add(LookupElementBuilder.create(namespace + "." + id));
-                        }
+
+        for (DomFileElement<SqlMap> fileElement : findSqlMapFileElements()) {
+
+            SqlMap rootElement = fileElement.getRootElement();
+            String namespace = rootElement.getNamespace().getRawText();
+            if (namespace != null) {
+                for (IdentifiableStatement statement : rootElement.getIdentifiableStatements()) {
+                    String id = statement.getId().getRawText();
+                    if (id != null) {
+                        result.add(LookupElementBuilder.create(namespace + "." + id));
                     }
                 }
             }
         }
 
         return result.toArray(new Object[result.size()]);
+
+    }
+
+    private List<DomFileElement<SqlMap>> findSqlMapFileElements() {
+        return DomService.getInstance().getFileElements(SqlMap.class, getElement().getProject(), GlobalSearchScope.projectScope(getElement().getProject()));
     }
 
 }
