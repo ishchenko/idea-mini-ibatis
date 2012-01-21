a69a81533286ab438b03ce216ce750f327e18e3c
Max Ishchenko <max.ishchenko@gmail.com>
2012-01-22 01:37:44 +0400

    delete, insert, update elements

---
+++ src/net/ishchenko/idea/minibatis/model/sqlmap/Delete.java
@@ -0,0 +1,10 @@
+package net.ishchenko.idea.minibatis.model.sqlmap;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Max
+ * Date: 22.01.12
+ * Time: 0:33
+ */
+public interface Delete extends SqlMapIdentifiableStatement {
+}
---
+++ src/net/ishchenko/idea/minibatis/model/sqlmap/Insert.java
@@ -0,0 +1,10 @@
+package net.ishchenko.idea.minibatis.model.sqlmap;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Max
+ * Date: 22.01.12
+ * Time: 0:34
+ */
+public interface Insert extends SqlMapIdentifiableStatement {
+}
