7f12ba057af23a38ddaa51ce374755f9c425d7da
Max Ishchenko <max.ishchenko@gmail.com>
2012-06-07 22:41:52 +0400

    iBATIS: multiple dots in fully qualified statement name support

--- src/net/ishchenko/idea/minibatis/IdentifiableStatementReference.java
+++ src/net/ishchenko/idea/minibatis/IdentifiableStatementReference.java
@@ -12,7 +12,9 @@ import com.intellij.util.xml.DomTarget;
 import net.ishchenko.idea.minibatis.model.sqlmap.SqlMapIdentifiableStatement;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -22,7 +24,7 @@ import java.util.regex.Pattern;
  * Time: 23:59
  */
 public class IdentifiableStatementReference extends PsiPolyVariantReferenceBase<PsiLiteralExpression> {
-    
+
     private static final Pattern dotPattern = Pattern.compile("\\.");
 
     public IdentifiableStatementReference(PsiLiteralExpression expression) {
@@ -40,40 +42,17 @@ public class IdentifiableStatementReference extends PsiPolyVariantReferenceBase<
             return ResolveResult.EMPTY_ARRAY;
         }
 
-        String[] parts = dotPattern.split(rawText.substring(1, rawText.length() - 1), 2);
+        String[] parts = dotPattern.split(rawText.substring(1, rawText.length() - 1));
 
-        String namespace;
-        String id;
-
-        if (parts.length == 2) {
-            namespace = parts[0];
-            id = parts[1];
+        if (parts.length == 1) {
+            return findResults("", parts[0]).toArray(new ResolveResult[0]);
         } else {
-            namespace = "";
-            id = parts[0];
-        }
-
-        CommonProcessors.CollectUniquesProcessor<SqlMapIdentifiableStatement> processor = new CommonProcessors.CollectUniquesProcessor<SqlMapIdentifiableStatement>();
-        ServiceManager.getService(getElement().getProject(), DomFileElementsFinder.class).processSqlMapStatements(namespace, id, processor);
-
-        Collection<SqlMapIdentifiableStatement> processorResults = processor.getResults();
-        final ResolveResult[] results = new ResolveResult[processorResults.size()];
-        final SqlMapIdentifiableStatement[] statements = processorResults.toArray(new SqlMapIdentifiableStatement[processorResults.size()]);
-        for (int i = 0; i < statements.length; i++) {
-            SqlMapIdentifiableStatement statement = statements[i];
-            DomTarget target = DomTarget.getTarget(statement);
-            if (target != null) {
-                XmlElement xmlElement = statement.getXmlElement();
-                final String locationString = xmlElement != null ? xmlElement.getContainingFile().getName() : "";
-                results[i] = new PsiElementResolveResult(new PomTargetPsiElementImpl(target) {
-                    @Override
-                    public String getLocationString() {
-                        return locationString;
-                    }
-                });
+            List<ResolveResult> results = new ArrayList<ResolveResult>();
+            for (int i = 0; i < parts.length - 1; i++) {
+                results.addAll(findResults(concatBefore(parts, i), concatAfter(parts, i + 1)));
             }
+            return results.toArray(new ResolveResult[results.size()]);
         }
-        return results;
 
     }
 
@@ -84,6 +63,52 @@ public class IdentifiableStatementReference extends PsiPolyVariantReferenceBase<
         ServiceManager.getService(getElement().getProject(), DomFileElementsFinder.class).processSqlMapStatementNames(processor);
         return processor.toArray(new String[processor.getResults().size()]);
 
+    }
+
+    private List<ResolveResult> findResults(String namespace, String id) {
+
+        CommonProcessors.CollectUniquesProcessor<SqlMapIdentifiableStatement> processor = new CommonProcessors.CollectUniquesProcessor<SqlMapIdentifiableStatement>();
+        ServiceManager.getService(getElement().getProject(), DomFileElementsFinder.class).processSqlMapStatements(namespace, id, processor);
+
+        Collection<SqlMapIdentifiableStatement> processorResults = processor.getResults();
+        final List<ResolveResult> results = new ArrayList<ResolveResult>(processorResults.size());
+        final SqlMapIdentifiableStatement[] statements = processorResults.toArray(new SqlMapIdentifiableStatement[processorResults.size()]);
+        for (SqlMapIdentifiableStatement statement : statements) {
+            DomTarget target = DomTarget.getTarget(statement);
+            if (target != null) {
+                XmlElement xmlElement = statement.getXmlElement();
+                final String locationString = xmlElement != null ? xmlElement.getContainingFile().getName() : "";
+                results.add(new PsiElementResolveResult(new PomTargetPsiElementImpl(target) {
+                    @Override
+                    public String getLocationString() {
+                        return locationString;
+                    }
+                }));
+            }
+        }
+        return results;
+    }
+
+    private String concatBefore(String[] parts, int before) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= before; i++) {
+            sb.append(parts[i]);
+            if (i + 1 <= before) {
+                sb.append(".");
+            }
+        }
+        return sb.toString();
+    }
+
+    private String concatAfter(String[] parts, int after) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = after; i < parts.length; i++) {
+            sb.append(parts[i]);
+            if (i + 1 < parts.length) {
+                sb.append(".");
+            }
+        }
+        return sb.toString();
     }
 
 }
